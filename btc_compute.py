44185102364cea8ce50717722814b57dcc3f9069
weishi <greatshi@live.cn>
2018-10-26 16:29:20 +0800

    compute for avalon 741

--- btc_compute.py
+++ btc_compute.py
@@ -2,11 +2,11 @@
 #coding=utf-8
 
 BTC_PRICE = 45000
-BTC_PER_HASH = 0.00003409
-HASH_RATE = 13.5
-round = 13
+BTC_PER_HASH = 0.0000339
+HASH_RATE = 7.5
+round_cycle = 13
 hard = 0.93
-KW = 1.4
+KW = 1.2
 electric = 0.3
 fee = KW * 24 * 13 * electric
 
@@ -22,18 +22,19 @@ def range_mode():
     earn = 0
     cost = 0
     for i in range(0, 100):
-        round_mine = BTC_PER_HASH * HASH_RATE * round * (hard ** i)
+        round_mine = BTC_PER_HASH * HASH_RATE * round_cycle * (hard ** i)
         earn += round_mine
         cost += fee
         earn -= fee/float(BTC_PRICE)
         if round_mine <= fee/float(BTC_PRICE):
-            print('{} month'.format(i*13/30.0))
+            print('{} month'.format(round(i*13/30.0, 5)))
             break
         if i == 99:
-            print('{} month'.format(i*13/30.0))
+            print('{} month'.format(round(i*13/30.0, 5)))
         print('round: {}, earn: {}, money: {}, total: {}'.format(
-              i+1, earn, round_mine*BTC_PRICE, earn*BTC_PRICE))
-    print('earn: {} cost: {}'.format(earn, cost))
+              i+1, round(earn, 5), round(round_mine*BTC_PRICE, 5),
+              round(earn*BTC_PRICE, 3)))
+    print('earn: {} cost: {}'.format(round(earn, 5), round(cost, 5)))
 
 
 def main():
